d31cc103395ae7f2df542d6fea9bc2b74f88329e
AkshitCD12 <159606777+AkshitCD12@users.noreply.github.com>
2026-04-05 15:47:42 +0530

    Add city selection dropdown to HeaderBar

--- Front-End/src/HeaderBar.jsx
+++ Front-End/src/HeaderBar.jsx
@@ -5,8 +5,19 @@ function HeaderBar() {
   return (
     <header>
       <h1>Air-Quality</h1>
-
+      
       <div id="header-button">
+            <div className="dropdown-container">
+                        <select value={city} onChange={(e) => setCity(e.target.value)}>
+                            <option value="" disabled>Choose City</option>
+                            <option value="Mumbai">Mumbai</option>
+                            <option value="Delhi">Delhi</option>
+                            <option value="Hyderabad">Hyderabad</option>
+                            <option value="Bangalore">Bangalore</option>
+                            <option value="Pune">Pune</option>
+                        </select>
+                    </div>
+
         <button className="btn no-margin">About</button>
         <button className="btn">More About</button>
       </div>
